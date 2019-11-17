5c14c52cb62abedb1398b61000f18403f7460ad6
howardg <howardg@hartwick.edu>
2019-11-16 21:58:37 -0500

    start

--- PersonClass.py
+++ PersonClass.py
@@ -1,3 +1,13 @@
 # Gavin H
 # 11/20/19
 # Person Class
+
+
+class Person:
+    def __init__(self, first, last, age):
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+
+    def description(self):
+        return "The name of the person is" + self.first_name + " " + self.last_name + " " + "and their age is" + str(self.age)
---
+++ Textbook.py
@@ -0,0 +1,11 @@
+# Gavin H
+# 11/20/19
+# Textbook Class
+
+from PersonClass import Person
+
+
+class Textbook:
+    def __init__(self, title, author, edition, isbn, publisher, year, quantity, price):
+        self.title = title
+        super().__init__(first, last, age) = author
